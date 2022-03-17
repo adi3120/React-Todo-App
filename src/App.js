3a47ba97f7151d0577797b53cf69f62505f73ce6
adi3120 <aditya.yadav.cs.20@ggits.net>
2022-03-17 23:53:49 +0530

    made the add button functional

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ import Plan from './Plan'
 import React, { Component } from 'react'
 
 class App extends Component {
+	state={
+		items:[],
+		text:"",
+	}
+
+	handleChange=e=>{
+		this.setState({text:e.target.value})
+	}
+	handleAdd=e=>{
+		if(this.state.text!==""){
+			const items=[...this.state.items,this.state.text]
+			this.setState({
+				items:items,
+				text:"",
+			})
+		}
+	}
   render() {
 	return (
 	  <div className='container-fluid my-5'>
@@ -18,14 +35,19 @@ class App extends Component {
 				  </h2>
 				  <div className="row">
 					  <div className="col-9">
-						  <input type="text" className='form-control' name="" id="" placeholder='Write Plan Here'/>
+						  <input type="text" className='form-control' name="" id="" placeholder='Write Plan Here'
+						  value={this.state.text} onChange={this.handleChange}/>
 					  </div>
 					  <div className="col-2">
-						  <button className="btn btn-warning px-5 fw-bold">Add</button>
+						  <button className="btn btn-warning px-5 fw-bold" onClick={this.handleAdd}>Add</button>
 					  </div>
 					  <div className="container-fluid">
 						  <ul className="list-unstyled row m-5">
-							<Plan/>
+							{
+								this.state.items.map((value,i)=>{
+									return <Plan value={value}/>
+								})
+							}
 						  </ul>
 					  </div>
 				  </div>
--- src/Plan.js
+++ src/Plan.js
@@ -1,8 +1,6 @@
 function Plan(props){
 	return <>
-	{console.log("Plan")}
-	<li>Plan 1</li>
-	<li>Plan 2</li>
+	<li>{props.value}</li>
 	</>
 }
 
